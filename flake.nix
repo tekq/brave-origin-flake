054cb0c0f42665c308729c0d0b8fc56e399f930b
asmx2 <asmx2@noreply.codeberg.org>
2026-06-28 16:42:00 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "brave-origin-flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          brave-origin = pkgs.callPackage ./package.nix { };
+          default = brave-origin;
+        });
+    };
+}
